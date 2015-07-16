f7e6bf6670437f8b3b9e7e834616d4e447d7dda5
Adrian McMenamin <adrianmcmenamin@gmail.com>
2015-07-16 22:29:16 +0100

    slow build up

--- processor.cpp
+++ processor.cpp
@@ -3,6 +3,38 @@
 
 using namespace std;
 
-Processor::Processor(Tile *parent): masterTile(parent)
+Processor::Processor(Tile *parent): masterTile(parent), mode(REAL)
+{}
+
+void Processor::switchModeReal()
 {
+	mode = REAL;
 }
+
+void Processor::switchModeVirtual()
+{
+	mode = VIRTUAL;
+}
+
+void Processor::load(const long regNo, const long value)
+{
+	if (regNo < 0 || regNo >= REGISTER_FILE_SIZE) {
+		throw "Bad register number";
+	}
+	long result = 0;
+	if (mode == REAL) {
+		//fetch physical address
+		//always runs
+	}
+	else if (mode == VIRTUAL) {
+		if (!mapped(value)) {
+			//simulate fault
+			//get mapping
+			//update pages etc
+		}
+		//now fetch virtual address
+	}
+
+	registerFile[regNo] = result;
+}
+		
--- processor.hpp
+++ processor.hpp
@@ -8,11 +8,15 @@ private:
 	std:vector<long> registerFile(REGISTER_FILE_SIZE);
 	long programCounter;
 	Tile *masterTile;
+	enum ProcessorMode { REAL, VIRTUAL };
+	ProcessorMode mode;
 public:
 	Processor(Tile* parent);
 	void load(const long regNo, const long value);
 	long read(const long regNo);
 	void loadMem(const long regNo, const long memAddr);
+	void switchModeReal();
+	void switchModeVirtual();
 	
 };
 
