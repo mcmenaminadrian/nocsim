d9e17989e5ad03c3ec7b38817e34875ec5f386ca
Adrian McMenamin <adrianmcmenamin@gmail.com>
2015-08-03 12:19:00 +0100

    added GCD code to solver

--- gjsolver.cpp
+++ gjsolver.cpp
@@ -11,6 +11,23 @@
 
 using namespace std;
 
+void gcd(pair<long, long>& input)
+{
+	long first = abs(input.first);
+	long second = abs(input.second);
+	if (first > second) {
+		first = second;
+		second = abs(input.first);
+	}
+	while (second != 0) {
+		long temp = second;
+		second = first%second;
+		first = temp;
+	}
+	input.first /= first;
+	input.second /= first;
+	cout << " GCD: " << first << "   ";
+}
 
 int main()
 {
@@ -40,28 +57,32 @@ int main()
 	}
 
 	for (int i = 0; i < lines.size(); i++) {
-		pair<long, long> pivot = (lines[i])[i];
-		pair<long, long> factor(pivot.second, pivot.first);
+		pair<long, long> factor(lines[i][i].second, lines[i][i].first);
+		for (int j = i; j <= lines.size(); j++) {
+			lines[i][j].first *= factor.first;
+			lines[i][j].second *= factor.second;
+		}
 		for (long j = i + 1; j < lines.size(); j++) {
-			pair<long, long> multiple(
-				(lines[j])[i].first * factor.first,
-				(lines[j])[i].second * factor.second);
+			pair<long, long> multiple(lines[j][i].first, lines[j][i].second);
 			cout << i << ":" << j << " --- factor is " << multiple.first << " / " << multiple.second << endl;
-			(lines[j])[i] = pair<long, long>(0, 0);
+			lines[j][i] = pair<long, long>(0, 0);
 			for (long k = i + 1; k <= lines.size(); k++) {
-				(lines[j])[k] = pair<long, long>( /* (lines[j])[k] - multiple * (lines[i][k] * factor) */
-					(lines[j])[k].first * multiple.second * (lines[i])[k].second - multiple.first * (lines[i])[k].first * (lines[j])[k].second,
-					(lines[j])[k].second * multiple.first * (lines[i])[k].first - multiple.second * (lines[i])[k].second * (lines[j])[k].first);
+				lines[j][k] = pair<long, long>( /* (lines[j])[k] - multiple * (lines[i][k] * factor) */
+					lines[j][k].first * multiple.second * lines[i][k].second - lines[i][k].first * multiple.first * lines[j][k].second,
+					lines[j][k].second * multiple.first * lines[i][k].first - lines[i][k].second * multiple.second * lines[i][k].first);
+				cout << "BEFORE: " << lines[j][k].first << " / " << lines[j][k].second;
+				gcd(lines[j][k]);
+				cout << "     AFTER: " << lines[j][k].first << " / " << lines[j][k].second << endl;
 			}
 		}
 	}
-//	cout << "DIAGONAL FORM" << endl;
-//	for (int i = 0; i < lines.size(); i++) {
-//		for (int j = 0; j < lines.size(); j++) {
-//			cout << lines[i][j].first << "/" << lines[i][j].second << " , ";
-//		}
-//		cout << " == " << lines[i][lines.size()].first << " / " << lines[i][lines.size()].second << endl;
-//	}
+	cout << "DIAGONAL FORM" << endl;
+	for (int i = 0; i < lines.size(); i++) {
+		for (int j = 0; j < lines.size(); j++) {
+			cout << lines[i][j].first << "/" << lines[i][j].second << " , ";
+		}
+		cout << " == " << lines[i][lines.size()].first << " / " << lines[i][lines.size()].second << endl;
+	}
 
 }
 		
